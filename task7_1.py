5a151bf9beadd98b66cfe8dc9c438310784e29e4
nandanreddy123 <58145682+nandanreddy123@users.noreply.github.com>
2020-11-29 11:14:43 +0530

    Add files via upload

---
+++ task7_1.py
@@ -0,0 +1,3 @@
+from task_module import task_list
+task_list.append(0)
+print(task_list)
---
+++ task7_2.py
@@ -0,0 +1,5 @@
+# importing pandas and converting a list into data frame
+import pandas as pd
+from task_module import task_list
+data_frame=pd.DataFrame(task_list)
+print(data_frame)
